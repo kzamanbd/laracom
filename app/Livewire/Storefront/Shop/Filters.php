1b7acb879a99a9715024510d7d8c0a0793992314
Kamruzzaman <kzamanbn@gmail.com>
2025-09-10 23:21:39 +0600

    Extract shop filters sidebar to Livewire component

---
+++ app/Livewire/Storefront/Shop/Filters.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Storefront\Shop;
+
+use Livewire\Component;
+
+class Filters extends Component
+{
+    public function render()
+    {
+        return view('livewire.storefront.shop.filters');
+    }
+}
