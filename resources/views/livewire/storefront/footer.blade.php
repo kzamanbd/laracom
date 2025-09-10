80aa37806cebc1d198ecadd5031ba7b23d4a8d7c
Kamruzzaman <kzamanbn@gmail.com>
2025-09-11 00:18:56 +0600

    Update footer.blade.php

--- resources/views/livewire/storefront/footer.blade.php
+++ resources/views/livewire/storefront/footer.blade.php
@@ -63,18 +63,18 @@
                 <div class="col-lg-2 col-md-3">
                     <h5 class="widget-title wow fadeIn animated">About</h5>
                     <ul class="footer-list wow fadeIn animated mb-sm-5 mb-md-0">
-                        <li><a href="#">About Us</a></li>
+                        <li><a href="{{ route('about') }}">About Us</a></li>
                         <li><a href="#">Delivery Information</a></li>
-                        <li><a href="#">Privacy Policy</a></li>
-                        <li><a href="#">Terms &amp; Conditions</a></li>
-                        <li><a href="#">Contact Us</a></li>
+                        <li><a href="{{ route('privacy-policy') }}">Privacy Policy</a></li>
+                        <li><a href="{{ route('terms-conditions') }}">Terms &amp; Conditions</a></li>
+                        <li><a href="{{ route('contact') }}">Contact Us</a></li>
                     </ul>
                 </div>
                 <div class="col-lg-2  col-md-3">
                     <h5 class="widget-title wow fadeIn animated">My Account</h5>
                     <ul class="footer-list wow fadeIn animated">
-                        <li><a href="my-account.html">My Account</a></li>
-                        <li><a href="#">View Cart</a></li>
+                        <li><a href="{{ route('my-account') }}">My Account</a></li>
+                        <li><a href="{{ route('cart') }}">View Cart</a></li>
                         <li><a href="{{ route('wishlist') }}">My Wishlist</a></li>
                         <li><a href="#">Track My Order</a></li>
                         <li><a href="#">Order</a></li>
