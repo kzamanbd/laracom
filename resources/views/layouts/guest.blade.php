2bb23a1830137fd79b48ee75325f7df9be765b8e
Kamruzzaman <kzamanbn@gmail.com>
2025-09-14 20:25:01 +0600

    Refactor guest layout to enhance UI with a radial gradient background and updated authentication card design. Improved logo display and overall structure for better user experience.

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -7,24 +8,25 @@
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
-            <div>
-                <a href="/" wire:navigate>
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-                </a>
-            </div>
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
-                {{ $slot }}
+    <body class="tw--radial-gradient">
+            <div class="flex min-h-screen items-center justify-center overflow-hidden p-6">
+                <div class="auth-card">
+                    <div class="auth-box-1"></div>
+                    <div class="auth-box-2"></div>
+                    <div class="card-body">
+                        <div class="my-4 flex items-center justify-center space-x-2">
+                            <x-application-logo class="size-9 fill-current text-black dark:text-white" />
+                            <p class="text-3xl font-semibold">{{ config('app.name', 'Laravel') }}</p>
+                        </div>
+                        {{ $slot }}
+                    </div>
+                </div>
             </div>
         </div>
     </body>
+
 </html>
