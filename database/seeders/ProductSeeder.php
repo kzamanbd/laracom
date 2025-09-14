5eb228100039efb09d0fd0a3723c37a78ae43b78
Kamruzzaman <kzamanbn@gmail.com>
2025-09-14 16:38:48 +0600

    Update ProductSeeder.php

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -17,6 +17,8 @@ use Illuminate\Support\Str;
 
 class ProductSeeder extends Seeder
 {
+    public $cachedCategories = [];
+
     public function storeImage(string $imageUrl, int $modelId, $collection = 'product_images')
     {
         try {
@@ -81,15 +83,8 @@ class ProductSeeder extends Seeder
                 $p = Product::find($p->id); // reload to get the casts
                 // attach categories if exists
                 if (! empty($product['category'])) {
-                    $category = Category::firstOrCreate([
-                        'slug' => Str::slug($product['category']),
-                        'parent_id' => null,
-                    ], [
-                        'name' => ucwords($product['category']),
-                        'description' => fake()->sentence(),
-                        'is_active' => true,
-                    ]);
-                    $p->categories()->syncWithoutDetaching([$category->id]);
+                    $categoryId = $this->getCategory($product['category']);
+                    $p->categories()->syncWithoutDetaching([$categoryId]);
                 }
                 // add product images
                 foreach ($product['images'] as $imageUrl) {
@@ -124,5 +119,23 @@ class ProductSeeder extends Seeder
         });
 
         $this->command->getOutput()->progressFinish();
+    }
+
+    public function getCategory($categoryName): int
+    {
+        if (isset($this->cachedCategories[$categoryName])) {
+            return $this->cachedCategories[$categoryName];
+        }
+        $category = Category::firstOrCreate([
+            'slug' => Str::slug($categoryName),
+            'parent_id' => null,
+        ], [
+            'name' => ucwords($categoryName),
+            'description' => fake()->sentence(),
+            'is_active' => true,
+        ]);
+        $this->cachedCategories[$categoryName] = $category->id;
+
+        return $category->id;
     }
 }
